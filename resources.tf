c80258d723eb3b6f98f502257c3c6dc9674b886e
Otto Quaglio <otto.q@gmx.com>
2022-10-26 06:45:00 +0800

    chore(resource): remove unused s3 object

--- resources.tf
+++ resources.tf
@@ -2,15 +2,6 @@ resource "random_pet" "this" {
   length = 2
 }
 
-resource "aws_s3_object" "hr_data" {
-  bucket                 = module.s3_bucket.s3_bucket_id
-  key                    = "sourcedata/hr/employee/LOAD0001.csv"
-  source                 = "data/hr.csv"
-  etag                   = filemd5("data/hr.csv")
-  server_side_encryption = "AES256"
-
-  tags = local.tags
-}
 
 # Role to allow DMS Service to access bucket
 resource "aws_iam_role" "s3_role" {
