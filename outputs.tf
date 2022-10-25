303cb273834820ef6ae8df4cf414db5385f9e917
Otto Quaglio <otto.q@gmx.com>
2022-10-25 12:06:59 +0800

    feat(output): add s3 role name as output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_name" {
   description = "Name of bucket"
   value       = module.s3_bucket.s3_bucket_id
 }
+
+output "s3_role_name" {
+  description = "Name of role"
+  value       = aws_iam_role.s3_role.name
+}
