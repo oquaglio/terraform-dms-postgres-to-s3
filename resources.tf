3eaa487b10758df69697bacfcea44c14f236d637
Otto Quaglio <otto.q@gmx.com>
2022-10-25 11:40:55 +0800

    feat(aws): add hr data as s3 object

--- resources.tf
+++ resources.tf
@@ -1,3 +1,13 @@
 resource "random_pet" "this" {
   length = 2
 }
+
+resource "aws_s3_object" "hr_data" {
+  bucket                 = module.s3_bucket.s3_bucket_id
+  key                    = "sourcedata/hr/employee/LOAD0001.csv"
+  source                 = "data/hr.csv"
+  etag                   = filemd5("data/hr.csv")
+  server_side_encryption = "AES256"
+
+  tags = local.tags
+}
