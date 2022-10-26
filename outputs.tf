a03af5997469621ea2906c4ad693d14fe1f51638
Otto Quaglio <otto.q@gmx.com>
2022-10-26 09:14:48 +0800

    feat(output): add aws account id to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
+}
+
 output "s3_bucket_name" {
   description = "Name of bucket"
   value       = module.s3_bucket.s3_bucket_id
