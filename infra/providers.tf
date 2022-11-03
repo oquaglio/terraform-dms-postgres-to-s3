616aefbfe6a231817ee8f2e734c6fc0bbc4bd4b9
Otto Quaglio <otto.q@gmx.com>
2022-11-03 15:02:46 +0800

    style: tweak formatting

--- infra/providers.tf
+++ infra/providers.tf
@@ -8,10 +8,10 @@ provider "time" {}
 
 provider "snowflake" {
   // required
-  username          = var.snowflake_user
-  account           = var.snowflake_account
-  region            = var.snowflake_region
-  private_key_path  = var.snowflake_private_key_path
+  username         = var.snowflake_user
+  account          = var.snowflake_account
+  region           = var.snowflake_region
+  private_key_path = var.snowflake_private_key_path
 
   // optional, at exactly one must be set
   #password = var.snowflake_password
